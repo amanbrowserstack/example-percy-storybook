07d935575c3b2cdf4d56000d114f1a778e2083d5
Aman Deswal <aman.d@browserstack.com>
2025-09-12 15:37:13 -0700

    add change 3

--- js/TodoFooter.js
+++ js/TodoFooter.js
@@ -34,6 +34,9 @@ export default function TodoFooter({
 
       {completedCount > 0 ? (
         <button 
+                style={{ 
+          fontWeight: 'bold', 
+        }} 
 
 
         className="clear-completed" 
